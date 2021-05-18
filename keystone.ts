22f78f6567bb377c7b9e5a04dc4730bb23d3ec2e
Jed Watson <jed.watson@me.com>
2021-05-18 11:50:32 +1000

    Updated to demo how to add the healthcheck to publicPages

--- keystone.ts
+++ keystone.ts
@@ -5,7 +5,8 @@ import * as lists from './schema';
 export default config({
   db: { adapter: 'prisma_sqlite', url: 'file:./app.db' },
   ui: {
-    isAccessAllowed: () => true,
+    isAccessAllowed: () => false,
+    publicPages: ['/healthcheck'],
   },
   experimental: {
     generateNextGraphqlAPI: true,
